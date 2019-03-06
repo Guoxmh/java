065c6bc5f4cffbbad07525bed547e1fa78b5c578
Guoxmh <1094185589@qq.com>
2019-03-06 15:33:22 +0800

    ii

--- Task2.java
+++ Task2.java
@@ -12,9 +12,12 @@ import java.util.Arrays;
 import java.util.Arrays;
 import java.util.Arrays;
 import java.util.Arrays;
+import java.util.ARRAYS;
 public class Task2{
 	public static void main(String[] args){
 		java.util.Arrays.sort();//快速排序；
+		java.util.Arrays.sort();
+		JAVA.UTIL.arrays.sort();
 		java.util.Arrays.sort();
 		java.util.Arrays.sort();
 		java.util.Arrays.sort();
@@ -34,13 +37,19 @@ public class Task2{
 		System.arraycopy();
 		System.arraycopy();
 		System.arraycopy();
+		System.arraycopy();
 		System.arraycopy(原数组,起始位置,目标数组,起始位置,拷贝长度);
 		Arrays.copyof();
 		Arrays.copyof(原数组,新数组长度);
 		Arrays.copyof(原数组,新数组长度);
 		Arrays.copyof(原数组,新数组长度);
 		Arrays.copyof(原数组,新数组长度);
+		Arrays.copyof(原数组,新数组长度);
 		Arrays.copyof();
+		Arrays.copyof();
+		Arrays.copyof();
+		Arrays.copyof();
+		java.util.Arrays.sort();
 		
 	}
 }
