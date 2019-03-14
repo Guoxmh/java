e961def56f14c8f28ec4be9ad259c8d00e36fb46
Guoxmh <1094185589@qq.com>
2019-03-14 23:41:53 +0800

    操作

---
+++ TestScanner.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public  class TestScanner{
+
+//Scanner的使用
+public static  void main(String[] args){
+      //输入
+	  Scanner scanner = new Scanner (System.in);
+	  //输出
+	  String name = scanner.nextLine();
+	  String  age = scanner.nextInt();
+	  String rs = scanner.next();
+	  rs.equals("sad");//
+}
+}
